6e7729b757ebd48669a78deaa55b62decfa15902
James Hurley <jamesghurley@gmail.com>
2018-08-03 23:50:22 +0200

    License

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject unpause/fdk-clj "1.0.0-SNAPSHOT"
   :description "Clojure FDK for Fn"
   :url "https://github.com/unpause-live/fdk-clj"
+  :license {:name "Apache License Version 2.0"
+        :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                 [cheshire "5.8.0"]
                 [clj-time "0.14.4"]
