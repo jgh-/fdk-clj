bede16b49675fc9436615bdec013aeb44dd071be
James Hurley <jamesghurley@gmail.com>
2018-08-05 15:56:26 +0200

    Add profiles for Clojure 1.7 through 1.10-SNAPSHOT and a test-all alias

--- project.clj
+++ project.clj
@@ -7,5 +7,14 @@
                 [cheshire "5.8.0"]
                 [clj-time "0.14.4"]
                 [org.clojure/test.check "0.9.0"]]
+  :min-lein-version "2.0.0"
+  :global-vars {*warn-on-reflection* true}
   :jvm-opts ["-Duser.timezone=UTC"]
-  :test-paths ["test"])
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
+                    :dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
+            }
+  :test-paths ["test"]
+  :aliases {"test-all" ["with-profile" "1.7,1.8,1.9,1.10" "test"]})
